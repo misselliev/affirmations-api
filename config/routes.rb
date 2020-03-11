7d2d13e61434295c80ec0a9df3904816d3de41ed
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-03-11 15:06:19 -0700

    Setting up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/affirmation/index', to: 'affirmation#index'
+  post '/affirmation/create', to: 'affirmation#create'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
